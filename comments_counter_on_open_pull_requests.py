87c58744d973e6f62f5903dad8bab5e6d646ae46
Omer Toledano <omer.toledano@kenshoo.com>
2014-03-31 08:55:45 +0300

    some minor changes
    Signed-off-by: Omer Toledano <omer.toledano@kenshoo.com>

--- comments_counter_on_open_pull_requests.py
+++ comments_counter_on_open_pull_requests.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 __author__ = 'omert'
 
 import requests
@@ -10,37 +12,34 @@ BASE_URL = 'https://api.github.com/repos/kenshoo'
 AUTH_TUPLE = (user, password)
 
 
-def get_resp(url, params={}):
+def get_response(url, params={}):
     return requests.get(url, auth=AUTH_TUPLE, params=params)
 
 
 def get_dict(url):
-    r = get_resp(url)
+    r = get_response(url)
     return json.loads(r.text)
 
 
-def get_list_from_resp(resp):
+def get_list_from_response(resp):
     l = json.loads(resp.text)
     if not resp.links.get('next', None):
         return l
-    return l + get_list_from_resp(requests.get(resp.links['next']['url'], auth=AUTH_TUPLE))
+    return l + get_list_from_response(requests.get(resp.links['next']['url'], auth=AUTH_TUPLE))
 
 
 def get_pulls_from_repo(repo_url):
-    return get_list_from_resp(get_resp(repo_url + '/pulls'))
+    return get_list_from_response(get_response(repo_url + '/pulls'))
 
 
 def get_commenters_hist_per_repo(comments_per_repo):
-    d = {}
+    commenters_per_repo = {}
     for repo, comments in comments_per_repo.iteritems():
-        d[repo] = {}
+        commenters_per_repo[repo] = defaultdict(lambda: 0)
         for comment in comments:
             commenter = comment['user']['login']
-            try:
-                d[repo][commenter] += 1
-            except KeyError:
-                d[repo][commenter] = 1
-    return d
+            commenters_per_repo[repo][commenter] += 1
+    return commenters_per_repo
 
 
 def get_comments_per_user(summerized_per_repo):
@@ -61,17 +60,15 @@ def summerize(commenters_hist_per_repo):
 
 
 def get_comments_per_repo(non_empty_pulls_per_repo):
-    d = {}
+    comments_per_repo = defaultdict(list)
     for repo_name, pulls_list in non_empty_pulls_per_repo.items():
-        d[repo_name] = []
-        for pull in pulls_list:
-            comments_url = pull['review_comments_url']
-            d[repo_name] += get_list_from_resp(get_resp(comments_url))
-    return d
+        comments_per_repo[repo_name] = reduce(lambda a, b: a+b,
+                                              [get_list_from_response(get_response(pull['review_comments_url'])) for pull in pulls_list],
+                                              [])
+    return comments_per_repo
 
-repositories = get_list_from_resp(get_resp('https://api.github.com/orgs/kenshoo/repos'))
-non_empty_pulls_per_repo = dict([(repo['name'], get_pulls_from_repo(repo['url']))
-                                 for repo in repositories if repo])
+repositories = get_list_from_response(get_response('https://api.github.com/orgs/kenshoo/repos'))
+non_empty_pulls_per_repo = dict([(repo['name'], get_pulls_from_repo(repo['url'])) for repo in repositories if repo])
 comments_per_repo = get_comments_per_repo(non_empty_pulls_per_repo)
 commenters_hist_per_repo = get_commenters_hist_per_repo(comments_per_repo)
 summary = summerize(commenters_hist_per_repo)
